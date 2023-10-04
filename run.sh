af4c6eb66ddd305dc7a545c67f0e3456128b7bdc
masira <masoud.iranmehr@qrtech.se>
2023-10-04 15:37:34 +0200

    add script

---
+++ run.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#./run.sh compile_flash prj01_cli_parser
+
+if [ $1 == 'compile_flash' ]
+then
+mkdir -p build
+cd build
+cmake -DPROJECT_CORE_FILES=$2 -DDO_FLASH=YES ..
+make -j11
+
+cd ..
+
+elif [ $1 == 'compile_only' ]
+then
+mkdir -p build
+cd build
+cmake -DPROJECT_CORE_FILES=$2 -DDO_FLASH=NO ..
+make -j11
+
+cd ..
+
+
+elif [ $1 == 'clean' ]
+then
+mkdir -p build
+cd build
+rm -r *
+echo 'cleaned successfully'
+cd ..
+
+fi
+
