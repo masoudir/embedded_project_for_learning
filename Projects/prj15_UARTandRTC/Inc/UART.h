8a56ed78ad659b66d454292ece954a213457389f
olivia <you@example.com>
2023-12-06 16:03:49 +0100

    rtc

---
+++ Projects/prj15_UARTandRTC/Inc/UART.h
@@ -0,0 +1,14 @@
+#ifndef __UART__H
+#define __UART__H
+
+#include <stdio.h>
+#include "stm32f4xx_hal.h"
+
+void MX_USART2_UART_Init(void);
+#define USART_TX_Pin GPIO_PIN_2
+#define USART_TX_GPIO_Port GPIOA
+#define USART_RX_Pin GPIO_PIN_3
+#define USART_RX_GPIO_Port GPIOA
+
+
+#endif /* __UART__H */
