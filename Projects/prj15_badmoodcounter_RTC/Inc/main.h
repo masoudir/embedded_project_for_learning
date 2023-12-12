9620dc3e4942b4393d941cc439a2e7b449e23800
olivia <you@example.com>
2023-12-12 16:46:05 +0100

    fixed the lcd

---
+++ Projects/prj15_badmoodcounter_RTC/Inc/main.h
@@ -0,0 +1,30 @@
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx_hal.h"
+#include"stm32f4xx_hal_spi.h"
+#include <stdbool.h>
+void MX_RTC_Init(void);
+void welcome_screen(void);
+void home_screen(void);
+void setting_screen(void);
+typedef enum {
+     LCD_mode_0,//welcome screen
+     LCD_mode_1,//home screen
+     LCD_mode_2,//setting
+} LCD_mode;
+
+void Error_Handler(void);
+
+
+
+#endif /* __MAIN_H */
