6a76ca8b851bd8d10ffbd5a0709103bd0fbec324
olivia <you@example.com>
2024-01-04 12:10:07 +0100

    fill the uart message

--- Projects/prj19_badmoodcounter_newcode/Src/badmood.c
+++ Projects/prj19_badmoodcounter_newcode/Src/badmood.c
@@ -14,6 +14,14 @@
 // extern RTC_TimeTypeDef gTime;
 // extern RTC_DateTypeDef  sDate;
 
+char BUFFER_time[16]={0};
+char BUFFER_date[16]={0};
+char BUFFER_welcome[100]={0};
+char BUFFER_line[100]={0};
+char BUFFER_homescreen[100]={0};
+char BUFFER_badmoodwords[100]={0};
+char BUFFER_7days[100]={0};
+char BUFFER_settingscreen[100]={0};
 
 void badmood_init(badmood_t * input){
     input->count=0;
@@ -39,8 +47,60 @@ void badmood_update_time_and_date(badmood_t * input, RTC_HandleTypeDef *hrtc_ptr
 
 void badmood_update_UART_screen(badmood_t *input, UART_HandleTypeDef *huart){
    if(input->lcdmode==LCD_mode_0){
-    char BUFFER[30]={0};
-    sprintf(BUFFER, "welcome");
-    HAL_UART_Transmit(huart,(const uint8_t*)BUFFER,30);
+    //char BUFFER_welcome[100]={0};
+    sprintf(BUFFER_welcome, "hi, welcome to badmoodcounter, please press key2 to continue\r\n");
+    HAL_UART_Transmit(huart, (const uint8_t*)BUFFER_welcome, 100);
+// show the time 
+
+    HAL_RTC_GetTime(&hrtc , &gTime , RTC_FORMAT_BIN);
+    sprintf (BUFFER_time,"%2.2d:%2.2d:%2.2d",gTime.Hours,gTime.Minutes,gTime.Seconds);
+    HAL_UART_Transmit(huart, (const uint8_t*)BUFFER_time, 16);
+
+    HAL_RTC_GetDate(&hrtc , &sDate , RTC_FORMAT_BIN);
+    sprintf (BUFFER_date,"20%2.2d.%2.2d.%2.2d", sDate.Year, sDate.Month, sDate.Date);
+    HAL_UART_Transmit(huart, (const uint8_t*)BUFFER_date, 16);              
+    
+    HAL_Delay(1000);
+
+
+//  show the separat line
+   // char BUFFER_line[100]={0};
+    sprintf(BUFFER_line, "-----------------------------------------------------------------\r\n");
+    HAL_UART_Transmit(huart, (const uint8_t*)BUFFER_line, 100);
    }
+
+
+
+    if(input->lcdmode==LCD_mode_1) {
+        sprintf(BUFFER_homescreen, "home screen current time \r\n");
+        HAL_UART_Transmit(huart, (const uint8_t*)BUFFER_homescreen, 100);
+
+
+    // show the time and date
+        HAL_UART_Transmit(huart, (const uint8_t*)BUFFER_time, 16);
+        HAL_UART_Transmit(huart, (const uint8_t*)BUFFER_date, 16);   
+
+    // show current badmoodcount
+
+        sprintf(BUFFER_badmoodwords, "current badmood is \r\n");
+        HAL_UART_Transmit(huart, (const uint8_t*)BUFFER_badmoodwords, 100);
+        HAL_UART_Transmit(huart, (uint8_t*)input->count, 100);
+
+     // show late seven days records
+        sprintf(BUFFER_7days, "Below is last 7days record\r\n");
+        HAL_UART_Transmit(huart, (const uint8_t*)BUFFER_7days, 100);
+
+       for(int i=0; i<7;i++) {    
+          HAL_UART_Transmit(huart, (uint8_t*)input->array, 100);
+        }
+  
+    } 
+
+
+
+
+   if(input->lcdmode==LCD_mode_2){
+        sprintf(BUFFER_settingscreen, "This is setting screen\r\n");
+        HAL_UART_Transmit(huart, (const uint8_t*)BUFFER_settingscreen, 100);
+    } 
 }
